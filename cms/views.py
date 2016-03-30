755b787f4fcee4061b470c5563951c8909883ba8
Lucia Villa Martinez <lucia.villa.martinez@gmail.com>
2016-03-30 16:37:31 +0200

    modificacion del fichero cms/views.py

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
 # Create your views here.
+
+def index(request, pagina):
+	try:
+		listado = Pages.objects.get(name=pagina)
+	except Pages.DoesNotExist:
+		return HttpResponse('Lo sentimos, esta pagina no ha sido almacenada.')
+	respuesta = str(listado.page)
+	return HttpResponse(respuesta)
+
+def pagina(request, indice):
+	try:
+		listado = Pages.objects.get(id=int(indice))
+	except Pages.DoesNotExist:
+		return HttpResponse('Lo sentimos, esta pagina no ha sido almacenada.')
+	respuesta = str(listado.page)
+	return HttpResponse(respuesta)
